37e8fd54fac722f1725b67f25a4d9c616d485bfb
frossi85 <frossi85@642cec71-ee0f-7f8f-377c-c724fa4837ab>
2011-09-15 10:31:46 +0000

    El arbol va creciendo, o escalando dependiendo de la edad Ya tiene los colores La distancia en la rama padre es aleatoria Se corrigieron alguns cositas

---  tp1-sistemas-graficos --username frossi85@gmail.com/SistemasGraficosTp1/src/arbol2/Arbol.java
+++  tp1-sistemas-graficos --username frossi85@gmail.com/SistemasGraficosTp1/src/arbol2/Arbol.java
@@ -2,6 +2,7 @@ package arbol2;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.ListIterator;
 import java.util.Random;
 
 import javax.media.opengl.GL2;
@@ -13,19 +14,26 @@ public class Arbol {
 	public float anguloFiMaximo = 35;//45;
 	public float anguloFiMinimo = 20;
 	public float anguloThetaMaximo = 180;
-	public float anguloThetaMinimo = 50 ;
+	public float anguloThetaMinimo = 50;
 	private ArrayList<Float> angulosFiPorNivel = new ArrayList<Float>();
 	private ArrayList<Float> angulosThetaPorNivel = new ArrayList<Float>();
 	private Iterator iteradorAngulosFi;
 	private Iterator iteradorAngulosTheta;
-
+	
+	public float ditanciaEnRamaMinima = 0.4f;
+	public float ditanciaEnRamaMaxima = 0.7f;
+	private ArrayList<Float> distanciasPorNivel = new ArrayList<Float>();
+	private Iterator iteradorDistancias;
+	
+	private float escalaPorEdad;
+	
 	public Arbol(float edad)
 	{
 		this.niveles = (int) edad;
 		this.edad = edad;
 		this.escala = this.edad - this.niveles; //Ver como la uso por q si multiplico se achica y puede quedar cero
 		
-		int cantParametrosACalcular = (int) (Math.pow(2, niveles) - 1);
+		int cantParametrosACalcular = (int) (Math.pow(2, niveles+1));
 		float signo = -1;
 		
 	 	for(int i = 0; i< cantParametrosACalcular; i++)
@@ -33,6 +41,7 @@ public class Arbol {
 	 		signo *= -1;
 	 		angulosFiPorNivel.add(signo*getAleatorio(anguloFiMinimo, anguloFiMaximo));
 	 		angulosThetaPorNivel.add(getAleatorio(anguloThetaMinimo ,anguloThetaMaximo));
+	 		distanciasPorNivel.add(getAleatorio(ditanciaEnRamaMinima ,ditanciaEnRamaMaxima));
 	 	}
 	}
 	
@@ -41,13 +50,13 @@ public class Arbol {
 		this.escala += velocidadCrecimiento;
 		
 		if(this.escala >= 1){
-			System.out.println("nuevo nivel : " + this.niveles);
+			//System.out.println("nuevo nivel : " + this.niveles);
 			this.niveles += (int) this.escala;
 			this.edad += (int) this.escala;
 			this.escala = this.escala - (int) this.escala;
 			  
 			//Ver como optmizar el calculo para no calcular potencias tan grandes
-			int cantParametrosACalcular = (int) (Math.pow(2, (int) this.niveles)) - angulosFiPorNivel.size() + 1;
+			int cantParametrosACalcular = (int) (Math.pow(2, (int) this.niveles+1));// - angulosFiPorNivel.size() + 1;
 			
 			float signo = -1;
 			
@@ -56,27 +65,24 @@ public class Arbol {
 		 		signo *= -1;
 		 		angulosFiPorNivel.add(signo*getAleatorio(anguloFiMinimo, anguloFiMaximo));
 		 		angulosThetaPorNivel.add(getAleatorio(anguloThetaMinimo ,anguloThetaMaximo));
+		 		distanciasPorNivel.add(getAleatorio(ditanciaEnRamaMinima ,ditanciaEnRamaMaxima));
 		 	}
 		}
 	}
 	
 	public void dibujar(GL2 gl)
-	{
-	   	gl.glTranslatef(0,0,-1);
-	   	gl.glRotatef(-angulosFiPorNivel.get(0),0.5f,1.0f,0.0f); //Fijarme si la rotacion no es con Fi
+	{	
+	   	gl.glTranslatef(-1,1,-1);
 	   	
 	   	iteradorAngulosFi = angulosFiPorNivel.iterator();
-	   	iteradorAngulosTheta = angulosThetaPorNivel.iterator();
+	   	iteradorAngulosTheta = angulosThetaPorNivel.listIterator();
+	   	iteradorDistancias = distanciasPorNivel.listIterator();
 	   	
-	   	//El atributo escala solo se usa por primera vbez para pasar el tamaño del tronco principal
+	   	escalaPorEdad = (float) Math.log10(this.edad);
 	   	
-	   	//PARA EL ESCALADO DEL CRECIMIENTO APLICAR aca un escalado de OPEN GL de todo el arbol
-	   	if(this.niveles == 1){
-	   		dibujarRecursivo(gl, true, new Rama(this.escala*3f, this.escala*0.25f, 20f), this.niveles);
-	   	}
-	   	else{
-	   		dibujarRecursivo(gl, true, new Rama(3f, 0.25f, 20f), this.niveles);
-	   	}
+	   	gl.glScalef(escalaPorEdad,escalaPorEdad,escalaPorEdad);
+	   			
+	   	dibujarRecursivo(gl, true, new Rama(3f, 0.25f, 20f), this.niveles);	   		
 	}
 	
 	private void dibujarRecursivo(GL2 gl, boolean tronco, Rama ramaPrincipal,  int nivel)
@@ -85,48 +91,45 @@ public class Arbol {
 		{
 			float anguloFi = 0;
 		    float anguloTheta = 0;
+		    float distancia1 = 0;
+		    float distancia2 = 0;
+		    float escala = 0.8f;
 		    
-		    //if(iteradorAngulosFi.hasNext())
+		   
 		    	anguloFi = (Float) iteradorAngulosFi.next();
-		    //if(iteradorAngulosFi.hasNext())
-		    	anguloTheta= (Float) iteradorAngulosTheta.next() ;
-			
+		    	anguloTheta = (Float) iteradorAngulosTheta.next();
+		    	distancia1 = (Float) iteradorDistancias.next();
+				distancia2 = (Float) iteradorDistancias.next();
+		    
 		    Rama r1;
 			
-			if(tronco)
-			{
-				r1 = ramaPrincipal;
-				gl.glPushMatrix();
-			}
-			else
-			{
-				if((nivel == 1)){
-					r1 = ramaPrincipal.getRamaEscalada(0.8f*this.escala);
-					gl.glPushMatrix();
-				   	gl.glTranslatef(0,0,ramaPrincipal.largo / 1.5f);
-				}
-				else {
-					r1 = ramaPrincipal.getRamaEscalada(0.8f);
-					gl.glPushMatrix();
-				   	gl.glTranslatef(0,0,ramaPrincipal.largo / 1.5f);
-				}
-			}
-		
-			r1.dibujar(gl, anguloFi, anguloTheta, ramaPrincipal.largo / 2);
-		
-			dibujarRecursivo(gl, false, r1, nivel - 1);
-			dibujarRecursivo(gl, false, r1, nivel - 1);
+			r1 = ramaPrincipal.getRamaEscalada(0.8f);
+			r1.dibujar(gl);
 			
-			if(nivel==1)
-			{
-				gl.glPushMatrix();
-				gl.glTranslatef(0,0,r1.largo);
-				new Hoja().dibujar(gl); //aca no va seguro q va despues de la traslacion de la rama
-				gl.glPopMatrix();
-			}
-				
+			gl.glPushMatrix();
+				gl.glTranslatef(0,0, (Float) iteradorDistancias.next() * ramaPrincipal.largo);
+			    gl.glRotatef((Float) iteradorAngulosFi.next(),0.5f,1f,0.0f);
+			    gl.glRotatef((Float) iteradorAngulosTheta.next(), 0f, 0f, 1f);
+				//gl.glScalef(escala,escala,escala);			    
+				dibujarRecursivo(gl, false, r1, nivel - 1);
+			gl.glPopMatrix();
+			gl.glPushMatrix();
+				gl.glTranslatef(0,0, (Float) iteradorDistancias.next() * ramaPrincipal.largo);
+			    gl.glRotatef(-(Float) iteradorAngulosFi.next(),0.5f,1f,0.0f);
+			    gl.glRotatef((Float) iteradorAngulosTheta.next(), 0f, 0f, 1f);
+			    //gl.glScalef(escala,escala,escala);
+				dibujarRecursivo(gl, false, r1, nivel - 1);				
 			gl.glPopMatrix();
 		}
+	
+	if(nivel==	0)
+	{
+		gl.glPushMatrix();
+		//gl.glTranslatef(0,0,r1.largo);
+		new Hoja().dibujar(gl); //aca no va seguro q va despues de la traslacion de la rama
+		gl.glPopMatrix();
+	}
+	
 	}
 	
 	private float getAleatorio(float min, float max)
---  tp1-sistemas-graficos --username frossi85@gmail.com/SistemasGraficosTp1/src/arbol2/Hoja.java
+++  tp1-sistemas-graficos --username frossi85@gmail.com/SistemasGraficosTp1/src/arbol2/Hoja.java
@@ -18,8 +18,8 @@ public class Hoja {
 		float tamanio = 0.2f;	
 
 		
-		gl.glColor3f(0.4f,0.7f,0.3f);
 		gl.glBegin(GL2.GL_TRIANGLE_STRIP);	
+			gl.glColor3f(0.4f,0.7f,0.3f);
 			gl.glVertex3f(0, -tamanio/2, tamanio);
 	  		gl.glVertex3f(0, 0, 2*tamanio);
 	  		gl.glVertex3f(0, tamanio/2, tamanio);
