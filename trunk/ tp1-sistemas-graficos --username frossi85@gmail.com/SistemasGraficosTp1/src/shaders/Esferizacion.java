6911d1d9db52cd6986e53515e359f2bdd48c4c66
gustavo.g.negri@gmail.com <gustavo.g.negri@gmail.com@642cec71-ee0f-7f8f-377c-c724fa4837ab>
2011-10-30 06:13:17 +0000

    agregado animacion

--- trunk/ tp1-sistemas-graficos --username frossi85@gmail.com/SistemasGraficosTp1/src/shaders/Esferizacion.java
+++ trunk/ tp1-sistemas-graficos --username frossi85@gmail.com/SistemasGraficosTp1/src/shaders/Esferizacion.java
@@ -9,22 +9,25 @@ public class Esferizacion {
 	private float centro[] = new float[3];
 	private float factor;
 	private float factorVariable = 0.0f;
+	private float time = 0.0f;
 	
 	private int memRadio = 1;
-	private int memCentro = 2;
-	private int memFactor = 3;
+	private int memCentro = 3;
+	private int memFactor = 2;
 		
 	public static String NOMBRE_FACTOR = "factor";
 	public static String NOMBRE_CENTRO = "centro";
 	public static String NOMBRE_RADIO = "radio";
+	public static String NOMBRE_TIME = "time";
 	
-	public Esferizacion(int radio, float x, float y,float z, float factor){
+	public Esferizacion(float radio, float x, float y,float z, float factor){
 		this.factor = factor;
 		this.radio = radio;
 		this.centro[0] = x;
 		this.centro[1] = y;
 		this.centro[2] = z;
 		this.factor = factor;
+		
 	}
 	
 	public void bind(GLAutoDrawable gLDrawable,int shaderprogram){
@@ -61,14 +64,21 @@ public class Esferizacion {
 	}
 	
 	public void update(){
-		if (factorVariable < factor){
-			factorVariable += 0.001f;
+		if (time < 1.0){
+			time += 0.005f;
 			
 		}
-		else
-			factorVariable = 0.0f;	
-		
 	}
+	
+	public float getTime(){
+		return this.time;
+				
+	}
+
+	public void setTime(float time){
+		this.time = time;
+	}
+
 	
 	public float getFactorVariable(){
 		return factorVariable;
