0b89abdd60e21d677b0a9399d7049afb732649f1
frossi85 <frossi85@gmail.com>
2011-11-24 02:56:52 +0000

    - Se comitea clase para el manejo del fragment general

---
+++  tp1-sistemas-graficos --username frossi85@gmail.com/SistemasGraficosTp1/src/shaders/FragmentGeneral.java
@@ -0,0 +1,10 @@
+package shaders;
+
+public class LucesFrag extends FragmentShader {
+	private String fileName = "luces.frag";
+	@Override
+	public String getFileName() {
+		return this.fileName;
+	}
+
+}
